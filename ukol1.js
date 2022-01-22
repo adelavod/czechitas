1b6ba300bd07b4d2c3992ce8f02b05b563276b25
adelavod <vod.adela@gmail.com>
2022-01-22 10:43:42 +0100

    1 file modified

--- ukol1.js
+++ ukol1.js
@@ -1,2 +1,7 @@
 
 console.log('THIS IS UKOL 1');
+let jmeno="Adela";
+let vek=(27);
+let rokNarozeni=(1994);
+let rok=vek+rokNarozeni;
+console.log(jmeno + ' a je mi ' + vek + " let a nyni je rok");
